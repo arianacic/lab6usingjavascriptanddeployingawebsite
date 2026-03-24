85813ad61a8a7133a36a2e18941b76da0cae9586
arianacic <ArianaCharlize.IzonCharles@edu.sait.ca>
2026-03-24 12:31:43 -0600

    create variables

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,4 @@
-/****************** YOUR NAME: 
+/****************** YOUR NAME: Ariana Charlize Izon-Charles+++++
 
 The instructions describe the missing logic that is needed; you will translate these into JavaScript in the places indicated.
 
@@ -11,6 +11,8 @@ You are encouraged to use the provided naming convention for ease of review.
 
 // INSERT YOUR CODE HERE
 
+let modelName = "test";
+let duration = 1;
 
 
 
